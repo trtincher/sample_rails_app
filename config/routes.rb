046b2f07233e8f35b356caab376fb91d85f9acbe
Travis Tincher <travistincher@Traviss-MacBook-Air.local>
2021-01-26 11:07:24 -0600

    finsh layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
